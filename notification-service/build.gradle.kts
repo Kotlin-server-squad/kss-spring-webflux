e2d26a1add03aee1c86e233fb38848eb4d6273ee
zezutom <zezulatomas@gmail.com>
2023-10-03 00:47:45 +0200

    chore: Gradle multi-module project - main root gradle config - minimalistic gradle config per subproject - config of r2dbc in order to pass tests - cleanup

--- notification-service/build.gradle.kts
+++ notification-service/build.gradle.kts
@@ -1,10 +1,9 @@
-plugins{
-    id("org.springframework.boot")
-    id("io.spring.dependency-management")
-    id("org.jetbrains.kotlin.plugin.allopen")
-    kotlin("jvm")
-    kotlin("plugin.spring")
-}
+val kotlinx_reactor_version: String by extra
+val jackson_version: String by extra
+val kotest_version: String by extra
+val mockk_version: String by extra
+val reactor_test_version: String by extra
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -12,11 +11,13 @@ dependencies {
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("org.postgresql:r2dbc-postgresql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.projectreactor:reactor-test")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.7.2")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.7.2")
-    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotest_version")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotest_version")
+    testImplementation("io.mockk:mockk:$mockk_version")
+    testImplementation("io.projectreactor:reactor-test:$reactor_test_version")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
 rootProject.name = "spring-webflux-coroutines"
 
-include("order-service","notification-service","delivery-service","client-service")
+include(":client-service")
+include(":delivery-service")
+include(":notification-service")
+include(":order-service")
