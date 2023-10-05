ac284db4028875a390cd4a832f9635fe7352af33
Jiří Hermann <jiri.hermann@gendigital.com>
2023-10-05 23:09:39 +0200

    add contracts for db

--- client-service/src/main/kotlin/cz/kss/proj/clientservice/entity/clientContract.kt
+++ client-service/src/main/kotlin/cz/kss/proj/clientservice/entity/clientContract.kt
@@ -1,5 +1,8 @@
 package cz.kss.proj.clientservice.entity
 
+import org.springframework.data.relational.core.mapping.Table
+
+@Table
 data class Client(
     val userName: String,
     val firstName: String,
@@ -7,6 +10,16 @@ data class Client(
 
 )
 
+@Table
 data class Address(
     val zipCode: Int,
+    val street: String,
+    val town: String,
+    val country: String,
+    val region: String,
 )
+
+
+
+
+
---
+++ order-service/src/main/kotlin/cz/kss/proj/orderservice/model/order-contract.kt
@@ -0,0 +1,11 @@
+package cz.kss.proj.orderservice.model
+
+
+import org.springframework.data.relational.core.mapping.Table
+
+@Table
+data class Item(
+
+    val id: Long,
+
+)
