48f52ff55b47be35585bd0d6156c5513de54aa4e
Jiří Hermann <jiri.hermann@gendigital.com>
2023-10-11 21:00:27 +0200

    update

--- order-service/src/main/resources/db/migration/V1__Init_DB.sql
+++ order-service/src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,64 +1,58 @@
+-- Create sequences for ID columns
+CREATE SEQUENCE item_id_seq;
+CREATE SEQUENCE item_category_id_seq;
+CREATE SEQUENCE k_order_id_seq;
+CREATE SEQUENCE order_line_item_id_seq;
+
 -- Create tables for entities
-
-
 CREATE TABLE "item_category"
 (
-    id          SERIAL PRIMARY KEY,
+    id          BIGINT DEFAULT nextval('item_category_id_seq') PRIMARY KEY,
     category    VARCHAR(255),
     description TEXT,
-    created_at TIMESTAMPTZ,
-    updated_at TIMESTAMPTZ
+    created_at  TIMESTAMPTZ,
+    updated_at  TIMESTAMPTZ
 );
 
 CREATE TABLE "item"
 (
-    id              SERIAL PRIMARY KEY,
-    name            VARCHAR(255),
-    description     TEXT,
-    price           NUMERIC,
-    count           INTEGER,
-    deleted         BOOLEAN,
-    item_category_id BIGINT,
-    created_at TIMESTAMPTZ,
-    updated_at TIMESTAMPTZ,
-    FOREIGN KEY (item_category_id) REFERENCES "item_category" (id)
+    id               BIGINT DEFAULT nextval('item_id_seq') PRIMARY KEY,
+    name             VARCHAR(255),
+    description      TEXT,
+    price            NUMERIC,
+    count            INTEGER,
+    deleted          BOOLEAN,
+    item_category_id BIGINT REFERENCES "item_category" (id),
+    created_at       TIMESTAMPTZ,
+    updated_at       TIMESTAMPTZ
 );
 
-CREATE TABLE "k-order"
+CREATE TABLE "k_order"
 (
-    id         SERIAL PRIMARY KEY,
-    price_sum   NUMERIC,
+    id         BIGINT DEFAULT nextval('k_order_id_seq') PRIMARY KEY,
+    priceSum   NUMERIC,
     status     VARCHAR(255),
     boolean    BOOLEAN,
-    customer_id BIGINT,
+    customerId BIGINT,
     created_at TIMESTAMPTZ,
     updated_at TIMESTAMPTZ
-
 );
 
 CREATE TABLE "order_line_item"
 (
-    id           SERIAL PRIMARY KEY,
+    id           BIGINT DEFAULT nextval('order_line_item_id_seq') PRIMARY KEY,
     count        INTEGER,
-    item_price    NUMERIC,
-    item_price_sum NUMERIC,
-    item_id      BIGINT,
-    order_id     BIGINT,
-    FOREIGN KEY (item_id) REFERENCES "item" (id),
-    FOREIGN KEY (order_id) REFERENCES "k-order" (id)
+    itemPrice    NUMERIC,
+    itemPriceSum NUMERIC,
+    item_id      BIGINT REFERENCES "item" (id),
+    order_id     BIGINT REFERENCES "k_order" (id),
+    created_at   TIMESTAMPTZ,
+    updated_at   TIMESTAMPTZ
 );
-
-CREATE TABLE "order-status"
-(
-    id     SERIAL PRIMARY KEY,
-    status VARCHAR(255)
-);
-
-
 
 -- Create indexes for improved query performance
 
-CREATE INDEX idx_order_customer_id ON "k-order" (customer_id);
+CREATE INDEX idx_item_item_category_id ON "item" (item_category_id);
 CREATE INDEX idx_order_line_item_item_id ON "order_line_item" (item_id);
 CREATE INDEX idx_order_line_item_order_id ON "order_line_item" (order_id);
-CREATE INDEX idx_item_item_category_id ON "item" (item_category_id);
+CREATE INDEX idx_order_customer_id ON "k_order" (customerId);
