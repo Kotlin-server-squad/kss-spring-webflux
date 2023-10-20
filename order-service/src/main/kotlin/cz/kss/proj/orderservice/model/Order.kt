3bc986933f591494b4533459137ab1f08ab0b41d
zezutom <zezulatomas@gmail.com>
2023-10-20 09:28:53 +0200

    SQL joins with r2dbc.

--- order-service/src/main/kotlin/cz/kss/proj/orderservice/model/Order.kt
+++ order-service/src/main/kotlin/cz/kss/proj/orderservice/model/Order.kt
@@ -1,6 +1,7 @@
 package cz.kss.proj.orderservice.model
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 import java.math.BigDecimal
 import java.time.Instant
@@ -8,15 +9,15 @@ import java.time.Instant
 @Table("k_order")
 data class Order(
     @Id
-    val id: Long,
-    val priceSum: BigDecimal,
-    val status: OrderStatus,
+    override var id: Long?,
+    @Column("priceSum") val priceSum: BigDecimal,
+    @Column("status") val status: OrderStatus,
     val boolean: Boolean,
     val orderLineItems: List<OrderLineItem>,
-    val customerId: Long,
-    val createdAt: Instant,
-    val updatedAt: Instant,
-)
+    @Column("customerId") val customerId: Long,
+    @Column("created_at") override var createdAt: Instant? = null,
+    @Column("updated_at") override var updatedAt: Instant? = null,
+) : BaseEntity<Long>
 
 enum class OrderStatus {
     NEW, COMPLETED, CANCELED
--- order-service/src/main/kotlin/cz/kss/proj/orderservice/repository/BaseRepository.kt
+++ order-service/src/main/kotlin/cz/kss/proj/orderservice/repository/BaseRepository.kt
@@ -8,24 +8,24 @@ import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.r2dbc.core.DatabaseClient.GenericExecuteSpec
 import org.springframework.r2dbc.core.awaitRowsUpdated
 
-interface BaseRepository<T : BaseEntity<I>, I> {
-    fun findAll(): Flow<T>
-    suspend fun findById(id: I): T?
-    suspend fun save(entity: T): T
-    suspend fun delete(entity: T): Boolean
+interface BaseRepository<ENTITY : BaseEntity<ENTITY_ID>, ENTITY_ID> {
+    fun findAll(): Flow<ENTITY>
+    suspend fun findById(id: ENTITY_ID): ENTITY?
+    suspend fun save(entity: ENTITY): ENTITY
+    suspend fun delete(entity: ENTITY): Boolean
     suspend fun deleteAll(): Boolean
 
     @Suppress("UNCHECKED_CAST")
-    suspend fun insert(entity: T, f: () -> GenericExecuteSpec): T {
+    suspend fun insert(entity: ENTITY, f: () -> GenericExecuteSpec): ENTITY {
         f().filter { statement: Statement, _ ->
             statement.returnGeneratedValues("id").execute()
         }.fetch().first().doOnNext { row ->
-            entity.id = row["id"] as I
+            entity.id = row["id"] as ENTITY_ID
         }.awaitSingleOrNull()
         return entity
     }
 
-    suspend fun update(entity: T, f: () -> GenericExecuteSpec): T {
+    suspend fun update(entity: ENTITY, f: () -> GenericExecuteSpec): ENTITY {
         return entity(f) ?: entity
     }
 
@@ -33,13 +33,31 @@ interface BaseRepository<T : BaseEntity<I>, I> {
         return f().fetch().awaitRowsUpdated() > 0
     }
 
-    suspend fun entity(f: () -> GenericExecuteSpec): T? {
+    suspend fun entity(f: () -> GenericExecuteSpec): ENTITY? {
         return f().fetch().first().mapNotNull { toEntity(it) }.awaitSingleOrNull()
     }
 
-    fun entities(f: () -> Flow<MutableMap<String, Any>>): Flow<T> {
+    fun entities(f: () -> Flow<MutableMap<String, Any>>): Flow<ENTITY> {
         return f().mapNotNull { toEntity(it) }
     }
 
-    fun toEntity(row: MutableMap<String, Any>): T?
+    fun toEntity(row: MutableMap<String, Any>): ENTITY?
+
+    @Suppress("UNCHECKED_CAST")
+    suspend fun <RELATED_ENTITY : BaseEntity<RELATED_ENTITY_ID>, RELATED_ENTITY_ID> toEntityWithRelationship(
+        rows: List<MutableMap<String, Any>>,
+        relatedIdColumn: String,
+        relatedEntitiesF: suspend (RELATED_ENTITY_ID) -> RELATED_ENTITY?,
+        withRelatedEntitiesF: (ENTITY, List<RELATED_ENTITY>) -> ENTITY
+    ): ENTITY? {
+        val entity = toEntity(rows.first()) ?: return null
+        val relatedEntities = rows.mapNotNull { row ->
+            if (row[relatedIdColumn] == null) {
+                null
+            } else {
+                relatedEntitiesF(row[relatedIdColumn] as RELATED_ENTITY_ID)
+            }
+        }
+        return if (relatedEntities.isEmpty()) entity else withRelatedEntitiesF(entity, relatedEntities)
+    }
 }
